e3ea27d27bc277cb8f57a14e6d9154536d97c687
ghosh sarbajit <37039890+ghosh-sarbajit@users.noreply.github.com>
2020-08-04 00:39:41 +0530

    Add files via upload

---
+++ russianpeaseant.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// A method to multiply two numbers using Russian Peasant method 
+unsigned int russianPeasant(long long a, long long b) 
+{ 
+	long long res = 0; // initialize result 
+
+	// While second number doesn't become 1 
+	while (b > 0) 
+	{ 
+		// If second number becomes odd, add the first number to result 
+		if (b & 1) 
+			res = res + a; 
+
+		// Double the first number and halve the second number 
+		a = a << 1; 
+		b = b >> 1; 
+	} 
+	return res; 
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	printf("%lld",russianPeasant(78645454545565,5457777777746));
+	return 0; 
+} 
